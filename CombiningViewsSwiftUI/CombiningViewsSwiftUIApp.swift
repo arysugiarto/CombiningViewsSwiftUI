98ad7b99220ce4e2c3727c13655e097729061171
arysugiarto <arysugiarto10@gmail.com>
2022-11-09 20:25:37 +0700

    Initial Commit

---
+++ CombiningViewsSwiftUI/CombiningViewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombiningViewsSwiftUIApp.swift
+//  CombiningViewsSwiftUI
+//
+//  Created by Ary Sugiarto on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CombiningViewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
